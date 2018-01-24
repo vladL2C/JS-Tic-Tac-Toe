04a202dd552706368871db478293bb8b11b001eb
Vlad <Vlad@hotmail.co.nz>
2018-01-24 22:15:26 +1300

    modal display tie

--- tictactoe.js
+++ tictactoe.js
@@ -16,30 +16,80 @@ function gameFactory(name1, name2) {
   }
 
   let currentPlayer = player1;
+  let turnCount = 0;
 
   function placeSymbol(square,index) {
         if (boardArray[index] !== "") return;
         
         if(currentPlayer === player1) {
-          let symbol = currentPlayer.symbol
-          square.textContent = symbol;
-          boardArray[index] = symbol;
+          square.textContent = currentPlayer.symbol;
+          boardArray[index] = currentPlayer.symbol;;
           currentPlayer = player2; 
           console.log(boardArray);
         } else {
-          let symbol = currentPlayer.symbol
-          square.textContent = symbol;
-          boardArray[index] = symbol;
+          square.textContent = currentPlayer.symbol;
+          boardArray[index] = currentPlayer.symbol;
           currentPlayer = player1; 
           console.log(boardArray);
         } 
+  }
+
+  let modal = document.querySelector('.modal');
+  function renderNewGame() {
+    boardArray = ["","","","","","","","",""];
+    modal.classList.remove('is-active');
+    console.log(boardArray);
+    gridSquares.forEach(square => square.innerHTML = '');
+    currentPlayer = player1;
+  }
+
+  
+  function winDisplay(player) {
+    modal.classList.add('is-active');
+    let winnerContent = document.querySelector('.thewinner');
+    winnerContent.textContent = `${player.name} is the winner!`;
+    let restartGame = document.querySelector('.button.is-info.is-large');
+    restartGame.addEventListener('click', renderNewGame);
+  }
+
+  function checkWin(board) {
+    let winner = false;
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    winConditions.forEach((win) => {
+      if (board[win[0]] === '') return;
+      if (board[win[0]] === board[win[1]]
+        && board[win[0]] === board[win[2]]) {
+        winner = true;
+        if (currentPlayer === player2) {
+          winDisplay(player1);
+        } else {
+          winDisplay(player2);
+        }
+      }
+    });
+
+    if (!boardArray.includes('') 
+        && winner === false )
+      console.log('tie cunt');
+
   }
 
   function play() {
     gridSquares.forEach((square,index) => {
       square.addEventListener('click', function() {
         placeSymbol(square, index);
-        console.log(index);
+        checkWin(boardArray);
+
       });
     });
 
@@ -51,6 +101,6 @@ function gameFactory(name1, name2) {
 
 }
 
-let vlad = playerFactory('Vlad','O');
-let game = gameFactory();
+
+let game = gameFactory('vlad','malika');
 game.play();
