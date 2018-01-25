98366f8fb5d8c97c098ed54591a4338e0aca4e5b
Vlad <Vlad@hotmail.co.nz>
2018-01-25 22:02:35 +1300

    refactored

--- tictactoe.js
+++ tictactoe.js
@@ -1,62 +1,55 @@
 function playerFactory(name, symbol) {
-  return {name, symbol};
+  return { name, symbol };
 }
 
-
 function gameFactory(name1, name2) {
-  let boardArray = ["","","","","","","","",""];
+  let boardArray = ["", "", "", "", "", "", "", "", ""];
 
-  if(name1 === "") name1 = "player 1";
-  if(name2 === "") name2 = "player 2";
+  if (name1 === "") name1 = "player 1";
+  if (name2 === "") name2 = "player 2";
 
   let player1 = playerFactory(name1, "X");
   let player2 = playerFactory(name2, "O");
 
-
-  const gridSquares = document.querySelectorAll('.gridsquare');
-  function drawGameBoard() {
-    gridSquares.forEach((square, index) => (square.innerHTML = boardArray[index]));
-  }
+  const gridSquares = document.querySelectorAll(".gridsquare");
 
   let currentPlayer = player1;
-  let turnPerson = document.querySelector('.turn');
-  turnPerson.textContent = currentPlayer.name +"'s" + "Turn!";
-  function placeSymbol(square,index) {
-        if (boardArray[index] !== "") return;
-        if(currentPlayer === player1) {
-          turnPerson.textContent = player2.name +"'s" + "Turn!";
-          square.textContent = currentPlayer.symbol;
-          boardArray[index] = currentPlayer.symbol;;
-          currentPlayer = player2; 
-        } else {
-          turnPerson.textContent = player1.name +"'s" + "Turn!";
-          square.textContent = currentPlayer.symbol;
-          boardArray[index] = currentPlayer.symbol;
-          currentPlayer = player1; 
-        } 
+  let turnPerson = document.querySelector(".turn");
+  turnPerson.textContent = currentPlayer.name + "'s" + "Turn!";
+  function placeSymbol(square, index) {
+    if (boardArray[index] !== "") return;
+    if (currentPlayer === player1) {
+      turnPerson.textContent = player2.name + "'s" + "Turn!";
+      square.textContent = currentPlayer.symbol;
+      boardArray[index] = currentPlayer.symbol;
+      currentPlayer = player2;
+    } else {
+      turnPerson.textContent = player1.name + "'s" + "Turn!";
+      square.textContent = currentPlayer.symbol;
+      boardArray[index] = currentPlayer.symbol;
+      currentPlayer = player1;
+    }
   }
 
-  const modal = document.querySelector('.modal');
+  const modal = document.querySelector(".modal");
   function renderNewGame() {
-    boardArray = ["","","","","","","","",""];
-    modal.classList.remove('is-active');
-    console.log(boardArray);
-    gridSquares.forEach(square => square.innerHTML = '');
+    boardArray = ["", "", "", "", "", "", "", "", ""];
+    modal.classList.remove("is-active");
+    gridSquares.forEach(square => (square.innerHTML = ""));
     currentPlayer = player1;
-    turnPerson.textContent = currentPlayer.name +"'s" + "Turn!";
+    turnPerson.textContent = currentPlayer.name + "'s" + "Turn!";
   }
 
-  
   function winDisplay(player) {
-    modal.classList.add('is-active');
-    let winnerContent = document.querySelector('.thewinner');
-    if (player.name) { 
+    modal.classList.add("is-active");
+    let winnerContent = document.querySelector(".thewinner");
+    if (player.name) {
       winnerContent.textContent = `${player.name} is the winner!`;
     } else {
       winnerContent.textContent = `It's a Tie!`;
-    } 
-    let restartGame = document.querySelector('.button.is-info.is-large');
-    restartGame.addEventListener('click', renderNewGame);
+    }
+    let restartGame = document.querySelector(".button.is-info.is-large");
+    restartGame.addEventListener("click", renderNewGame);
   }
 
   function checkWin(board) {
@@ -72,10 +65,9 @@ function gameFactory(name1, name2) {
       [2, 4, 6]
     ];
 
-    winConditions.forEach((win) => {
-      if (board[win[0]] === '') return;
-      if (board[win[0]] === board[win[1]]
-        && board[win[0]] === board[win[2]]) {
+    winConditions.forEach(win => {
+      if (board[win[0]] === "") return;
+      if (board[win[0]] === board[win[1]] && board[win[0]] === board[win[2]]) {
         winner = true;
         if (currentPlayer === player2) {
           winDisplay(player1);
@@ -85,40 +77,32 @@ function gameFactory(name1, name2) {
       }
     });
 
-    if (!boardArray.includes('') 
-        && winner === false )
-      winDisplay('Tie');
-
+    if (!boardArray.includes("") && winner === false) winDisplay("Tie");
   }
 
   function play() {
-    gridSquares.forEach((square,index) => {
-      square.addEventListener('click', function() {
+    gridSquares.forEach((square, index) => {
+      square.addEventListener("click", function() {
         placeSymbol(square, index);
         checkWin(boardArray);
       });
     });
-
   }
 
-  
-
-  return {play}
-
+  return { play };
 }
 
-const form = document.querySelector('form');
-const playNow = document.querySelector('.playnow');
-
+const form = document.querySelector("form");
+const playNow = document.querySelector(".playnow");
 
 function initializeGame() {
-  const gameBoard = document.querySelector('.gameboard');
-  playNow.addEventListener('click', function() {
-    gameBoard.classList.add('fade');
-    let game = gameFactory(form.name1.value,form.name2.value);
+  const gameBoard = document.querySelector(".gameboard");
+  playNow.addEventListener("click", function() {
+    gameBoard.classList.add("fade");
+    let game = gameFactory(form.name1.value, form.name2.value);
     form.parentNode.removeChild(form);
     game.play();
-  })
+  });
 }
 
 initializeGame();
